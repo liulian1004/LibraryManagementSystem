3a218eda93b8dc335c27aac65794bea2f010d5c5
Lian <liulian1004@hotmail.com>
2022-01-14 22:45:17 -0800

    init project

---
+++ src/bu/cs622/Main.java
@@ -0,0 +1,8 @@
+package bu.cs622;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ test/bu/cs622/MainTest.java
@@ -0,0 +1,16 @@
+package bu.cs622;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class MainTest {
+
+    @Test
+    public void xTest(){
+        assertTrue(true);
+    }
+
+
+}
